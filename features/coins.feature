237e6208ed0ac32c1c6d4eab9cc67474a1e85b5a
Nicholas Shiell <nicholas@nshiell.com>
2015-03-20 10:55:25 +0000

    Working UI and Behat tests

---
+++ features/coins.feature
@@ -0,0 +1,34 @@
+Feature: Calculate coins for amount
+  In order to calculate the the coins needed for an amount
+  As a website user
+  I need to be able to input an amount and see the coins
+
+  Scenario: Inputting in bad data
+    Given I am on "/"
+    When I fill in "amount" with "abcd"
+    And I press "calculate"
+    Then I should see "The amount \"abcd\" has no coins"
+
+  Scenario: Inputting valid data
+    Given I am on "/"
+    When I fill in "amount" with "7.73"
+    And I press "calculate"
+    Then the "table.coins .coin.coin-X2" element should contain "£2"
+    And the "table.coins .coin.coin-X1" element should contain "£1"
+    And the "table.coins .coin.coin-50p" element should contain "50p"
+    And the "table.coins .coin.coin-20p" element should contain "20p"
+    And the "table.coins .coin.coin-2p" element should contain "2p"
+    And the "table.coins .coin.coin-1p" element should contain "1p"
+
+  @javascript
+  Scenario: Inputting valid data with lightbox
+    Given I am on "/"
+    When I fill in "amount" with "7.73"
+    And I press "calculate"
+    And I wait for the lighbox to appear
+    Then the ".fancybox-outer table.coins .coin.coin-X2" element should contain "£2"
+    And the ".fancybox-outer table.coins .coin.coin-X1" element should contain "£1"
+    And the ".fancybox-outer table.coins .coin.coin-50p" element should contain "50p"
+    And the ".fancybox-outer table.coins .coin.coin-20p" element should contain "20p"
+    And the ".fancybox-outer table.coins .coin.coin-2p" element should contain "2p"
+    And the ".fancybox-outer table.coins .coin.coin-1p" element should contain "1p"
---
+++ views/cache/3a/4d/6ad2649e93c52d5571c55c920b8540f7f2a7421ce79288b91b5c8cb06296.php
@@ -0,0 +1,98 @@
+<?php
+
+/* index.html.twig */
+class __TwigTemplate_3a4d6ad2649e93c52d5571c55c920b8540f7f2a7421ce79288b91b5c8cb06296 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class=\"no-js lt-ie9 lt-ie8 lt-ie7\"> <![endif]-->
+<!--[if IE 7]>         <html class=\"no-js lt-ie9 lt-ie8\"> <![endif]-->
+<!--[if IE 8]>         <html class=\"no-js lt-ie9\"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class=\"no-js\"> <!--<![endif]-->
+<html lang=\"en\" class=\"no-js\">
+    <head>
+        <meta charset=\"utf-8\">
+        <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+        <title>NShiell: PHP Coin Calculator</title>
+        <meta name=\"description\" content=\"Money Calculator\" />
+        <meta name=\"keywords\" content=\"Money\" />
+        <!-- <meta name=\"author\" content=\"Peter Finlan\" /> -->
+        <meta name=\"author\" content=\"Nicholas Shiell\" />
+        <!-- Bootstrap -->
+        <script src=\"js/modernizr.custom.js\"></script>
+        <link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">
+        <link href=\"css/jquery.fancybox.css\" rel=\"stylesheet\">
+        <link href=\"css/flickity.css\" rel=\"stylesheet\" >
+        <link href=\"css/animate.css\" rel=\"stylesheet\">
+        <link href=\"http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css\" rel=\"stylesheet\">
+        <link href='http://fonts.googleapis.com/css?family=Nunito:400,300,700' rel='stylesheet' type='text/css'>
+        <link href=\"css/styles.css\" rel=\"stylesheet\">
+        <link href=\"css/queries.css\" rel=\"stylesheet\">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src=\"https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js\"></script>
+        <script src=\"https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js\"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+        <p class=\"browsehappy\">You are using an <strong>outdated</strong> browser. Please <a href=\"http://browsehappy.com/\">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <!-- open/close -->
+        <header>
+            <section class=\"hero\">
+                <div class=\"texture-overlay\"></div>
+                <div class=\"container\">
+                    <div class=\"row hero-content\">
+                        <div class=\"col-md-12\">
+                            <h1 class=\"animated fadeInDown\">A simple PHP application that will work out the minimum number of sterling coins needed to a make a total </h1>
+                            <p>Enter the amount of money</p>
+                            <form action=\"#\">
+                                <input class=\"animated fadeInUp\" type=\"text\" />
+                                <a class=\"use-btn animated fadeInUp\" href=\"http://tympanus.net/codrops/?p=22554\">Work out coins needed</a>
+                                <!--<a href=\"#about\" class=\"learn-btn animated fadeInUp\">Learn more <i class=\"fa fa-arrow-down\"></i></a>-->
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </header>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src=\"js/min/toucheffects-min.js\"></script>
+        <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\"></script>
+        <script src=\"js/flickity.pkgd.min.js\"></script>
+        <script src=\"js/jquery.fancybox.pack.js\"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src=\"js/retina.js\"></script>
+        <script src=\"js/waypoints.min.js\"></script>
+        <script src=\"js/bootstrap.min.js\"></script>
+        <script src=\"js/min/scripts-min.js\"></script>
+    </body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
